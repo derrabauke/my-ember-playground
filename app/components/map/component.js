55553f4fa207f4e6cef8f6e217b3cfb6540fc741
derrabauke <pfralf@googlemail.com>
2021-07-08 22:01:50 +0200

    Change test folder structure to pods layout too

--- app/components/map/component.js
+++ app/components/map/component.js
@@ -1,4 +1,7 @@
 import Component from '@glimmer/component';
-
+import ENV from 'em-app/config/environment';
 export default class MapComponent extends Component {
+  get token() {
+    return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
+  }
 }
