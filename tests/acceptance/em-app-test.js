475d9ab165e61e9654dbe8f34b9b94238b248217
derrabauke <pfralf@googlemail.com>
2021-07-08 18:22:01 +0200

    Introduce NavBar component - including tests - introduced it into templates

--- tests/acceptance/em-app-test.js
+++ tests/acceptance/em-app-test.js
@@ -9,6 +9,8 @@ module('Acceptance | em app', function (hooks) {
     await visit('/');
 
     assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Super Rentals');
     assert.dom('h2').hasText('Welcome to my Ember App!');
 
     assert.dom('.jumbo a.button').hasText('About Us');
@@ -21,6 +23,8 @@ module('Acceptance | em app', function (hooks) {
     await visit('/about');
 
     assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Super Rentals');
     assert.dom('h2').hasText('About this project');
 
     assert.dom('.jumbo a.button').hasText('Get in touch');
@@ -33,11 +37,31 @@ module('Acceptance | em app', function (hooks) {
     await visit('/getting-in-touch');
 
     assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Super Rentals');
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('.jumbo a.button').hasText('About Us');
     await click('.jumbo a.button');
 
     assert.equal(currentURL(), '/about');
+  });
+
+  test('navigating using the nav-bar', async function (assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('Super Rentals');
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
   });
 });
